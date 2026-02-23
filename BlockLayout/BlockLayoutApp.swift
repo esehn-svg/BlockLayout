13ff99882a903fcdd447844b31d6ce18084d493d
Ella Sehn <esehn@lcs.on.ca>
2026-02-22 21:00:15 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Ella Sehn on 2026-02-22.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
